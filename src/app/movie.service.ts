66d6cf79b83248edd46b4023d3c14a31b3d7a1b2
Paul Leavitt <ven.pleavitt@goodstart.org.au>
2019-11-26 17:36:56 +1000

    First Commit

---
+++ src/app/movie.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private moviesUrl = 'https:// developers.themoviedb.org/3';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+}
